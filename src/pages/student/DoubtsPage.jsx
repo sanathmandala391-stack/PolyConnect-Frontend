6aa716d52e0f7c4b661bbe9a3b598c532d4d9573
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-09-13 09:50:08 +0530

    Update DoubtsPage.jsx

--- src/pages/student/DoubtsPage.jsx
+++ src/pages/student/DoubtsPage.jsx
@@ -8,7 +8,7 @@
 
 // // Candidate models for highest availability
 // const CANDIDATE_MODELS = [
-"gemini-2.5-flash",
+// "gemini-2.5-flash",
 //   "gemini-2.0-flash",
 //   "gemini-1.5-flash",
 //   "gemini-1.5-pro",
